046bfe8032d44d79a4373992ea5fa642f5311044
Ahmed Salah <elshitehi1@gmail.com>
2025-04-17 05:18:31 +0200

    the routes and the app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ProductsComponent} from './pages/products/products.component';
+import {BuyComponent} from './pages/buy/buy.component';
+import {HomeComponent} from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'buy/:id', component: BuyComponent},
+];
